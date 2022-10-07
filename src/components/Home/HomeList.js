a00547cb53bdfeacc7571b15f7f5f5fb9b2d0238
techEmprez <choiceosobor@gmail.com>
2022-10-07 13:58:32 +0100

    fetch data

--- src/components/Home/HomeList.js
+++ src/components/Home/HomeList.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import HomeItem from './HomeItem';
+import { fetchCoins } from '../../redux/CryptoSlice';
+import Search from './Search';
 
 const HomeList = () => {
   const [query, setQuery] = useState('');
@@ -7,10 +11,26 @@ const HomeList = () => {
     setQuery(event.target.value);
   };
 
-   const cryptos = useSelector((state) => state.crypto);
+  const cryptos = useSelector((state) => state.crypto);
 
   const filteredCrypto = cryptos.filter((crypto) => (
     crypto.name.toLowerCase().includes(query.toLowerCase())
   ));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, [dispatch, fetchCoins]);
+
+  return (
+    <>
+      <Search query={query} onSearch={onSearch} />
+      <ul className="homeList">
+        { filteredCrypto.map((crypto) => <HomeItem key={crypto.id} crypto={crypto} />)}
+      </ul>
+    </>
+  );
+};
 
 export default HomeList;
