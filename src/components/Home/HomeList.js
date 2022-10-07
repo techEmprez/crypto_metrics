36b13cd197bbcab2352bdbd4fa38658287d011a0
techEmprez <choiceosobor@gmail.com>
2022-10-07 14:08:07 +0100

    fetch data from API

--- src/components/Home/HomeList.js
+++ src/components/Home/HomeList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import HomeItem from './HomeItem';
 import { fetchCoins } from '../../redux/CryptoSlice';
--- src/components/Home/Search.js
+++ src/components/Home/Search.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Search = () => (
-  <div />
-);
+function Search(props) {
+  const { query, onSearch } = props;
+
+  return (
+    <div className="searchContainer">
+      <input
+        type="text"
+        placeholder="search coin"
+        value={query}
+        onChange={onSearch}
+      />
+    </div>
+  );
+}
+
+Search.propTypes = {
+  query: PropTypes.string.isRequired,
+  onSearch: PropTypes.func.isRequired,
+};
 
 export default Search;
