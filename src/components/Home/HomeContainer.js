c91702e938a1e03967f36ebe3dfbeb8d259cffbc
techEmprez <choiceosobor@gmail.com>
2022-10-07 04:33:32 +0100

    create file structure

---
+++ src/components/Home/HomeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeList from './HomeList';
+import './App.css';
+
+const HomeContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeContainer
