ea47c6650eac3c4489ea0aed21471748bfd3a590
techEmprez <choiceosobor@gmail.com>
2022-10-07 12:50:10 +0100

    setup app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeContainer from './components/Home/HomeContainer';
+import DetailsContainer from './components/DetailsContainer';
+import Navigation from './components/Navigation';
 
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route exact path="/" element={<HomeContainer />} />
+          <Route path="/details/:id" element={<DetailsContainer />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
